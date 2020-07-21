fc3edd7c9ce1db3035383ddb167520f6850eaafd
buffon2110 <30846486+buffon2110@users.noreply.github.com>
2020-07-21 08:08:19 +0200

    Create jsjs2.js

---
+++ jsjs2.js
@@ -0,0 +1,1 @@
+<script type="text/javascript">window.NREUM||(NREUM={});NREUM.info={"beacon":"bam.nr-data.net","licenseKey":"62915533ca","applicationID":"14035018","transactionName":"blEHMktWXkQABkRQDFcbMBRQGFFbFQpCVhZNURdISV9A","queueTime":0,"applicationTime":38,"atts":"QhYERANMTUo=","errorBeacon":"bam.nr-data.net","agent":""}</script>
